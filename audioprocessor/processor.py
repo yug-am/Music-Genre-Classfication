0d35adcc0737406140c85634ca01d43b58451660
Yugam <yugam11parashar@gmail.com>
2023-03-16 23:34:34 +0530

    :sparkles:Audio to MFCC json function

---
+++ audioprocessor/processor.py
@@ -0,0 +1,41 @@
+import json
+import os
+import math
+import librosa
+
+
+
+def mfcc_process(dataset_output_path, json_output_path, sampling_per_track, sampling_rate, duration, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
+   
+    data_set = {
+        "genre_dir": [],
+        "label": [],
+        "mfcc": []
+    }
+
+    sampling_segment = int(sampling_per_track / num_segments)
+    expected_mfcc_count = math.ceil(sampling_segment / hop_length)
+
+    for i, (dirpath, dirnames, tracknames) in enumerate(os.walk(dataset_output_path)):
+
+        if dirpath is not dataset_output_path:
+            genre_label = dirpath.split("/")[-1]
+            data_set["genre_dir"].append(genre_label)
+            for f in tracknames:
+                curr_file_path = os.path.join(dirpath, f)
+                signal, sample_rate = librosa.load(curr_file_path, sr=sampling_rate)
+                for d in range(num_segments):
+                    start = sampling_segment * d
+                    finish = start + sampling_segment
+                    temp_signal = signal[start:finish]
+                    mfcc = librosa.feature.mfcc(y=temp_signal, sr= sample_rate, n_mfcc=num_mfcc, n_fft=n_fft,
+                                                hop_length=hop_length)
+                    mfcc_t = mfcc.T
+                    if len(mfcc_t) == expected_mfcc_count:
+                        data_set["mfcc"].append(mfcc_t.tolist())
+                        data_set["label"].append(i - 1)
+                        print("{}, segment:{}".format(curr_file_path, d + 1))
+    with open(json_output_path, "w") as fp:
+        json.dump(data_set, fp, indent=4)
+
+
--- main.py
+++ main.py
@@ -3,14 +3,19 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
+from audioprocessor.processor import mfcc_process
+def genre_classifier():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    dataset_output_path = "data/raw_data"
+    json_output_path = "data/json_data/data.json"
+    sample_rate = 22050
+    audio_duration = 30  # GTZAN project
+    samples_per_track = sample_rate * audio_duration
+    mfcc_process(dataset_output_path, json_output_path, samples_per_track, sample_rate, audio_duration, num_segments = 10)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("Main function call")
+    #   genre_classifier()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
